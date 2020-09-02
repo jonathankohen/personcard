95c5ac8a52a003c399fcfe50b977cbf95ecc42e2
Jon Kohen <jonkohen@Jons-Mac-mini.local>
2020-09-02 16:13:14 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <PersonCard
+                firstName="Jane"
+                lastName="Doe"
+                age="45"
+                harColor="Black"
+            />
+            <PersonCard
+                firstName="John"
+                lastName="Smith"
+                age="88"
+                harColor="Brown"
+            />
+            <PersonCard
+                firstName="Millard"
+                lastName="Fillmore"
+                age="50"
+                harColor="Brown"
+            />
+            <PersonCard
+                firstName="Maria"
+                lastName="Smith"
+                age="62"
+                harColor="Brown"
+            />
+        </div>
+    );
 }
 
 export default App;
