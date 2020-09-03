c7210b75afacae94fd56ac32bd0dbf97d01dba38
Jon Kohen <jonkohen@Jons-Mac-mini.local>
2020-09-03 14:51:54 -0400

    first commit

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age,
+        };
+    }
+
+    addYear = () => {
+        this.setState((prevState) => {
+            return { age: prevState.age + 1 };
+        });
+    };
+
     render() {
         return (
-            <>
-                <h1>
-                    {this.props.lastName}, {this.props.firstName}
-                </h1>
-                <p>Age: {this.props.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
-            </>
+            <div className="card">
+                <div className="card-body">
+                    <h4 className="card-title">
+                        {this.props.lastName}, {this.props.firstName}
+                    </h4>
+                    <p className="card-text">{this.state.age}</p>
+                    <p className="card-text">
+                        Hair Color: {this.props.hairColor}
+                    </p>
+                    <button onClick={this.addYear}>
+                        Birthday Button for {this.props.firstName}{' '}
+                        {this.props.lastName}
+                    </button>
+                </div>
+            </div>
         );
     }
 }
